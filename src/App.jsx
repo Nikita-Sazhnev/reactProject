340eaac1f083c418126f37638724969af6e6e9ec
Nikita-Sazhnev <nikitas4478@gmail.com>
2021-08-15 20:21:39 +0300

    'selfhooks'

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useMemo } from 'react';
-import PostFrom from './components/PostFrom';
-import PostList from './components/PostList';
-import PostFilter from './components/PostFilter';
-import MyModal from './components/UI/Modal/MyModal';
-import MyButton from './components/UI/Button/MyButton';
+import React, { useState, useMemo } from 'react'
+import {usePosts} from './hooks/usePosts'
+import PostFrom from './components/PostFrom'
+import PostList from './components/PostList'
+import PostFilter from './components/PostFilter'
+import MyModal from './components/UI/Modal/MyModal'
+import MyButton from './components/UI/Button/MyButton'
 
 
 import "./styles/App.css"
@@ -19,25 +20,14 @@ function App() {
   const [filter, setFilter] = useState({sort: '', query: ''})
   const [modal, setModal] = useState(false)
 
-  
-  const sortedPosts = useMemo(() => {
-    if(filter.sort)
-      return [...posts].sort((a,b) => a[filter.sort].localeCompare(b[filter.sort]))
-    return posts
-  }, [filter.sort, posts])
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((p) => {
-      return p.title.toLowerCase().includes(filter.query.toLowerCase()) || 
-             p.body.toLowerCase().includes(filter.query.toLowerCase())
-    })
-  }, [filter.query, sortedPosts])
+  const sortedAndSearchedPosts =  usePosts(posts, filter.sort, filter.query)
 
   const createPost = (newPost) =>  {
     setPosts([...posts, newPost])
     setModal(false)
   }
   const removePost = (post) => setPosts(posts.filter(p => p.id !== post.id))
+
 
   return (
     <div className="App">
