a2b2faa1bbd925915eccfcb7fc8edba9113a8927
Nikita-Sazhnev <nikitas4478@gmail.com>
2021-08-15 20:08:26 +0300

    'reactanimation'

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,4 +1,5 @@
 import PostItem from './PostItem';
+import {TransitionGroup, CSSTransition} from 'react-transition-group'
 
 const PostList = ({posts, remove}) =>{ 
 
@@ -8,9 +9,17 @@ const PostList = ({posts, remove}) =>{
     return (
       <div>
         <h1 style={{textAlign: 'center'}}>Список постов</h1>
+        <TransitionGroup>
         {posts.map((post, index) => 
-            <PostItem remove={remove} number={index+1} post={post} key={post.id}/>
+          <CSSTransition
+            key={post.id}
+            timeout={500}
+            classNames="post"
+          >
+            <PostItem remove={remove} number={index+1} post={post}/>
+          </CSSTransition>
         )}
+        </TransitionGroup>
       </div>
     );
 };
